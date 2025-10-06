3add6eb309477d5fe98e16e4e2be66b9a01149cb
thyildirim <taha0yldrm@outlook.com>
2025-10-06 13:09:04 +0300

    feat: add web config

---
+++ user-service/src/main/java/com/example/user_service/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.user_service;
+
+import com.example.user_service.util.JwtInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/**");
+    }
+}
